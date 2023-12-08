1046352856502e1ae58b0d1d5628738c7f989fa5
Maria Jose Meneses Velasco <mjmeneses1@espe.edu.ec>
2023-12-07 23:51:39 -0500

    interface TasaInteresRepository

---
+++ banking/src/main/java/com/banquito/core/banking/dao/TasaInteresRepository.java
@@ -0,0 +1,10 @@
+package com.banquito.core.banking.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.banquito.core.banking.domain.TasaInteres;
+
+public interface TasaInteresRepository extends CrudRepository<TasaInteresRepository, Long> {
+    TasaInteres findbyTipoTasaInteres(String tipo);
+
+}
