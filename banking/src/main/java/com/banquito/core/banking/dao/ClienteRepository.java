2befa132d6536e5fda94205213491a3ad4c66cca
Hector David Jimenez Armas <davidjimenezamh5o@gmail.com>
2023-12-11 13:55:02 -0500

    ActualizacionClases
    Actualizo clases

--- banking/src/main/java/com/banquito/core/banking/dao/ClienteRepository.java
+++ banking/src/main/java/com/banquito/core/banking/dao/ClienteRepository.java
@@ -1,5 +1,19 @@
 package com.banquito.core.banking.dao;
 
-public interface ClienteRepository {
+import org.springframework.data.repository.CrudRepository;
+
+import com.banquito.core.banking.domain.Cliente;
+import java.util.List;
+
+
+public interface ClienteRepository extends CrudRepository<Cliente, Long>{
+
+    Cliente findfindByTipoIdentificacionAndNumeroIdentificacion (String tipoIdentificacion, String numeroIdentificacion);
+    Cliente findfindByCorreoElectronico(String correoElectronico);
+    List <Cliente> findByTelefono(String telefono);
+    
+
+    
+
 
 }
--- banking/src/main/java/com/banquito/core/banking/dao/TipoRelacionRepository.java
+++ banking/src/main/java/com/banquito/core/banking/dao/TipoRelacionRepository.java
@@ -2,6 +2,8 @@ package com.banquito.core.banking.dao;
 
 import org.springframework.data.repository.CrudRepository;
 
-public interface TipoRelacionRepository extends CrudRepository(){
+import com.banquito.core.banking.domain.TipoRelacion;
+
+public interface TipoRelacionRepository extends CrudRepository<TipoRelacion, String>{
 
 }
