046280924c7aed4ead68714ea188efb82548d62c
Maria Jose Meneses Velasco <mjmeneses1@espe.edu.ec>
2023-12-11 18:19:14 -0500

    create CreditoTablaPagosRepository,java

---
+++ banking/src/main/java/com/banquito/core/banking/dao/CreditoTablaPagosRepository.java
@@ -0,0 +1,10 @@
+package com.banquito.core.banking.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.banquito.core.banking.domain.CreditoTablaPagos;
+
+public interface CreditoTablaPagosRepository extends CrudRepository<CreditoTablaPagosRepository, Long> {
+
+    CreditoTablaPagos findbyFechaPlanificadPagoAndEstado(Date fechaPlanificadaPago, String estado);
+}
