743f1190966e1e14d7b9b6461461631a70ecc37c
Maria Jose Meneses Velasco <mjmeneses1@espe.edu.ec>
2023-12-11 18:16:42 -0500

    create CuentaIntervinientesRepository.java

---
+++ banking/src/main/java/com/banquito/core/banking/dao/CuentaIntervinientesRepository.java
@@ -0,0 +1,7 @@
+package com.banquito.core.banking.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CuentaIntervinientesRepository extends CrudRepository<CuentaIntervinientesRepository, Long> {
+
+}
