34dd0de3ec4c9df5ef4073b1008b20812320083a
Maria Jose Meneses Velasco <mjmeneses1@espe.edu.ec>
2023-12-15 14:49:52 -0500

    create class Cliente

---
+++ banking-app/src/app/clases/Cliente/cliente.ts
@@ -0,0 +1,15 @@
+export class Cliente {
+  codCliente: number;
+  tipoCliente: string;
+  tipoIdentificacion: string;
+  numeroIdentificacion: string;
+  apellidos: string;
+  nombres: string;
+  fechaNacimiento: Date;
+  razonSocial: string;
+  nombreComercial: string;
+  fechaConstitucion: Date;
+  direccion: string;
+  correoElectronico: string;
+  telefono: string;
+}
