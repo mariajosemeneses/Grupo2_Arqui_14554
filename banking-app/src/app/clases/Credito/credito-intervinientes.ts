e821a7f9a77e209588a8725e23a85a95d97dceeb
Maria Jose Meneses Velasco <mjmeneses1@espe.edu.ec>
2023-12-15 15:11:54 -0500

    create class

---
+++ banking-app/src/app/clases/Credito/credito-intervinientes.ts
@@ -0,0 +1,9 @@
+import { Cliente } from "../Cliente/cliente";
+import { Credito } from "./credito";
+
+export class CreditoIntervinientes {
+  codCredito: Credito;
+  codCliente: Cliente;
+  tipo: string;
+}
+
---
+++ banking-app/src/app/clases/Credito/credito.ts
@@ -0,0 +1,19 @@
+import { Cliente } from "../Cliente/cliente";
+import { TipoCredito } from "./tipo-credito";
+
+export class Credito {
+  codCredito: number;
+  codTipoCredito: TipoCredito;
+  codCliente: Cliente;
+  numeroOperacion: string;
+  fechaCreacion: Date;
+  monto: number;
+  plazo: number;
+  tasaInteres: number;
+  estado: string;
+  fechaDesembolso: Date;
+  fechaUltimoPago: Date;
+  capitalPendiente: number;
+  fechaUltimoCambio: Date;
+}
+
