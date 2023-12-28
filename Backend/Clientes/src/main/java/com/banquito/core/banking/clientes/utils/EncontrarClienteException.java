f0a791b2356c3bfe2602b4ea4d5b6d905247f7c6
Sebastian Jaramillo <hsjj2000@gmail.com>
2023-12-28 01:14:37 -0500

    Actualizacion carpeta

--- Backend/Clientes/src/main/java/com/banquito/core/banking/clientes/utils/EncontrarClienteException.java
+++
@@ -1,8 +0,0 @@
-package com.banquito.core.banking.clientes.utils;
-
-public class EncontrarClienteException extends RuntimeException {
-
-    public EncontrarClienteException(String message) {
-        super(message);
-    }
-}
--- Backend/Clientes/src/main/java/com/banquito/core/banking/clientes/utils/ValidacionException.java
+++
@@ -1,8 +0,0 @@
-package com.banquito.core.banking.clientes.utils;
-
-public class ValidacionException extends RuntimeException {
-
-    public ValidacionException(String message) {
-        super("Valor incorrecto en " + message);
-    }
-}
